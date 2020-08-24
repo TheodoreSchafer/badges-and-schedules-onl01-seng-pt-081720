6a0c638e3be9d477815e061a23543d76f5716dcb
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-24 16:13:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,8 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendees|
+    puts badge_maker
+end
